a4ad27334107ab6808569b9f28a74e18ad8b8977
Théo Vuilliomenet <theo.vuilliomenet@he-arc.ch>
2023-03-09 11:20:33 +0100

    #2 link keys to actions

---
+++ Assets/Script/Driving.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour
+{
+    private float speed;
+
+    void Start()
+    {
+        speed = 5f;
+    }
+
+    void Update()
+    {
+        // Gets the input informations
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0.25f, 0));
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -0.25f, 0));
+        }
+    }
+}
