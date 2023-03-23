54cf8739ebe9a134951ce3f5efc6f8a5a880e793
Théo Vuilliomenet <theo.vuilliomenet@he-arc.ch>
2023-03-23 10:42:32 +0100

    #2 Improve driving and add acceleration simulation

--- Assets/Script/Driving.cs
+++ Assets/Script/Driving.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Driving : MonoBehaviour
 {
-    private float speed;
+    private float baseSpeed;
+    private float baseRotation;
 
     void Start()
     {
-        speed = 5f;
+        baseSpeed = 10f;
+        baseRotation = 0.5f;
     }
 
     void Update()
     {
+        // V2 : Driving with the axis and the keys and a simulation of acceleration
+        float speed = Input.GetAxis("Vertical");
+        float direction = Input.GetAxis("Horizontal");
+
+        if (speed != 0)
+        {
+            transform.Translate(Vector3.forward * baseSpeed * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(new Vector3(0, direction * baseRotation, 0));
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(new Vector3(0, direction * baseRotation, 0));
+            }
+        }
+        
+
+        // V1 : driving just with the keys
+        /*
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -30,5 +53,6 @@ public class Driving : MonoBehaviour
         {
             transform.Rotate(new Vector3(0, -0.25f, 0));
         }
+        */
     }
 }
